6d083565ab8997e05a69433ebd2be781cc8b03a1
shahiadarsh <141950380+shahiadarsh@users.noreply.github.com>
2024-08-16 23:55:39 +0530

    Update Vite.js

--- Vite.js
+++ Vite.js
@@ -4,8 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel([
-            'resources/css/app.css',
-            'resources/js/app.html',
+            'Portfolio.css',
+            'Protfolio.html',
         ]),
     ],
 });
